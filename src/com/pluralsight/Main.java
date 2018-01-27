08aecf834150124d1f72f3a242e5459653a635ff
Kiki <kiki_e@hotmail.com>
2018-01-27 18:53:59 -0500

    Initial commit

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+public class Main {
+    /*
+    https://programmingbydoing.com/
+    Basic arrays 0 - Assignment #138
+     */
+
+    public static void main(String[] args) {
+
+    Integer[] integers = new Integer[10];
+
+        integers[0] = -133;
+        integers[1] = -133;
+        integers[2] = -133;
+        integers[3] = -133;
+        integers[4] = -133;
+        integers[5] = -133;
+        integers[6] = -133;
+        integers[7] = -133;
+        integers[8] = -133;
+        integers[9] = -133;
+
+
+        System.out.println("Slot 1 contains a " + integers[0]);
+        System.out.println("Slot 2 contains a " + integers[1]);
+        System.out.println("Slot 3 contains a " + integers[2]);
+        System.out.println("Slot 4 contains a " + integers[3]);
+        System.out.println("Slot 5 contains a " + integers[4]);
+        System.out.println("Slot 6 contains a " + integers[5]);
+        System.out.println("Slot 7 contains a " + integers[6]);
+        System.out.println("Slot 8 contains a " + integers[7]);
+        System.out.println("Slot 9 contains a " + integers[8]);
+        System.out.println("Slot 10 contains a " + integers[9]);
+
+
+    }
+}
